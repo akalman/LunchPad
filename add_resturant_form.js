04ee1147c4086a2610dc78d264184aece4e6e08c
John Emau <jemau@daptiv.com>
2013-12-12 13:56:26 -0800

    wire up adding lunch spot

--- add_resturant_form.js
+++ add_resturant_form.js
@@ -1,5 +1,13 @@
 if (Meteor.isClient) {
-
+    Template.add_lunch_spot.events({
+        'click input': function (event) {
+            Restaurants.insert({
+                name: $('#restaurant_name').val()
+            });
+            window.location.hash = '';
+            $('#restaurant_name').val('');
+        }
+    });
 }
 
 if (Meteor.isServer) {
