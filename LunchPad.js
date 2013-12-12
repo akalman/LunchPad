9025cedcb0b137f8f48f1676b736ba5ef8b1af86
dkeka <dkeka@brisingamen.local>
2013-12-12 12:07:14 -0800

    list of restaurant.

--- LunchPad.js
+++ LunchPad.js
@@ -1,34 +1,25 @@
 
 Restaurants = new Meteor.Collection("restaurants");
-Players = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
+    Template.restaurants.restaurants = function() {
+        return Restaurants.find({});
+    }
 
-//    Template.hello.events({
-//        'click input' : function () {
-//            // template data, if any, is available in 'this'
-//            if (typeof console !== 'undefined')
-//                console.log("You pressed the button");
-//        }
-//    });
-//
-//    Template.addRestaurantButton.events({
-//        'click input' : function () {
-//            // template data, if any, is available in 'this'
-//            Restaurants.insert({
-//                name: 'rrrrRRRRRRed Robin (yum)'
-//            });
-//
-//            console.log('restaurant added.')
-//        }
-//    });
+    Template.addRestaurantButton.events({
+        'click input' : function () {
+            // template data, if any, is available in 'this'
+            Restaurants.insert({
+                name: 'rrrrRRRRRRed Robin (yum)'
+            });
+
+            console.log('restaurant added.')
+        }
+    });
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
         // code to run on server at startup
-        Players.insert({
-            name: 'John'
-        });
     });
 }
