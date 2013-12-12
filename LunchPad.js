2adea0f4353992bd207a01d2b0a020f4867a07ec
dkeka <dkeka@brisingamen.local>
2013-12-12 12:45:30 -0800

    users working.

--- LunchPad.js
+++ LunchPad.js
@@ -1,9 +1,23 @@
 
 Restaurants = new Meteor.Collection("restaurants");
+Assignments = new Meteor.Collection("assignments");
+Users = new Meteor.Collection("users");
 
 if (Meteor.isClient) {
     Template.restaurants.restaurants = function() {
         return Restaurants.find({});
+    }
+
+    Template.restaurant.users = function() {
+        var assignments = Assignments.find({
+            restaurantId: this._id
+        });
+
+        return Users.find({
+            _id: {$in: assignments.map(function(assignment) {
+                return assignment.userId;
+            })}
+        });
     }
 
     Template.addRestaurantButton.events({
