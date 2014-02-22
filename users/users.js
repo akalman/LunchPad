c66fec32ed857ff45f12c87836de1fe41c919416
dkeka <dkeka.zkfaks+github@gmail.com>
2014-02-21 21:07:59 -0800

    fixed bug where only current user's gravatar would show.

--- users/users.js
+++ users/users.js
@@ -1,29 +1,15 @@
 (function() {
-    // UI Plugs
-    if (Meteor.isClient) {
-        Deps.autorun(function(){
-            Meteor.subscribe('userData');
-        });
-    }
-
-    // Set Up
     if (Meteor.isServer) {
         Accounts.onCreateUser(function(options, user) {
-            user.gravatarUrl =
+            var gravatarUrl =
                 'http://www.gravatar.com/avatar/' +
                     CryptoJS.MD5(user.emails[0].address.toLowerCase()) +
                     '?d=monsterid&s=40';
 
             user.profile = options.profile || {};
+            user.profile.gravatarUrl = gravatarUrl;
 
             return user;
-        });
-
-        Meteor.publish('userData', function() {
-            if(!this.userId) return null;
-            return Meteor.users.find(this.userId, {
-                fields: { gravatarUrl: 1 }
-            });
         });
     }
 })();
